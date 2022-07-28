125d2076f629aa78e9b1a876c9df91a15edc6fc5
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-07-28 19:34:13 -0300

    feat: set up logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use tracing::info;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::Registry;
+
 fn main() {
-  println!("Hello, world!");
+  std::env::set_var(
+    "RUST_LOG",
+    std::env::var("RUST_LOG").unwrap_or_else(|_| format!("{}=info", env!("CARGO_PKG_NAME"))),
+  );
+
+  let (non_blocking_writer, _guard) = tracing_appender::non_blocking(std::io::stdout());
+
+  let app_name = concat!(env!("CARGO_PKG_NAME"), "-", env!("CARGO_PKG_VERSION")).to_string();
+
+  let bunyan_formatting_layer = BunyanFormattingLayer::new(app_name, non_blocking_writer);
+
+  let subscriber = Registry::default()
+    .with(JsonStorageLayer)
+    .with(bunyan_formatting_layer);
+
+  tracing::subscriber::set_global_default(subscriber).unwrap();
+
+  info!("Hello, world! caraio");
 }
